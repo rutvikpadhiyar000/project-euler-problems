e7950d4d775d92b5dcc51d4a2acfd10b0c849c7c
rutvik <rutvikpadhiyar000@gmail.com>
2020-12-30 15:49:07 +0530

    Solved question 3

--- 3. Largest prime factor/ans.py
+++ 3. Largest prime factor/ans.py
@@ -1,6 +1,6 @@
 from math import sqrt
 
-n = 600851475143 # Number of which we want greatest prime factor.
+n = 4376596150763457 # Number of which we want greatest prime factor.
 
 ans = -1 # initializing with not smallest value.
 
@@ -8,10 +8,13 @@ ans = -1 # initializing with not smallest value.
 # is already odd we dont need to check even
 # numbers.
 # 
-# since any prime factor  
-for i in range(3, int(sqrt(n)), 2):
+# since any prime factor 
+for i in range(3, int(sqrt(n)) + 1, 2):
     while n % i == 0:
         ans = i
         n /= i
+    
+if n > 2: 
+    ans = n
 
-print(ans)
+print(int(ans))
